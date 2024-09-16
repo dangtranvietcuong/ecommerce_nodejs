e1870a10de689c5adeb0a8c582fd3930e5ee4e26
dangtranvietcuong <cuongcesc12a1@gmail.com>
2024-09-16 15:48:54 +0700

    setup env and config

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./src/app");
 
-const PORT = 3055;
+const PORT = process.env.PORT || 3056;
 
 const server = app.listen(PORT, () => {
     console.log(`Web server ecommerce start with ${PORT}`);
 })
 
-process.on('SIGINT', () => {
-    server.close(() => console.log(`Exit server express`));
-    // notify.send(...ping)
-})
+// process.on('SIGINT', () => {
+//     server.close(() => console.log(`Exit server express`));
+//     // notify.send(...ping)
+// })
