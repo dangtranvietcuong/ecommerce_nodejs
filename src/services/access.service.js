4c3231e1177ec0052cd4d3784132bfa6c3930c23
dangtranvietcuong <cuongcesc12a1@gmail.com>
2024-09-18 14:41:51 +0700

    create access token and refesh token with level ease

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -35,39 +35,38 @@ class AccessService {
 
       if (newShop) {
         // created privateKey, publicKey
-        const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-          modulusLength: 4096,
-          publicKeyEncoding: {
-            type: 'pkcs1',
-            format: 'pem'
-          },
-          privateKeyEncoding: {
-            type: 'pkcs1',
-            format: 'pem'
-          }
-        });
-        // const privateKey = crypto.getRandomValues(64).toString('hex')
-        // const publicKey = crypto.getRandomValues(64).toString('hex')
+        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+        //   modulusLength: 4096,
+        //   publicKeyEncoding: {
+        //     type: 'pkcs1',
+        //     format: 'pem'
+        //   },
+        //   privateKeyEncoding: {
+        //     type: 'pkcs1',
+        //     format: 'pem'
+        //   }
+        // });
+        const privateKey = crypto.randomBytes(64).toString('hex')
+        const publicKey = crypto.randomBytes(64).toString('hex')
         // Public key CryptoGraphy Standards
         console.log({ privateKey, publicKey });
 
-        const publicKeyString = await KeyTokenService.createKeyToken({
+        const keyStore = await KeyTokenService.createKeyToken({
           userId: newShop._id,
           publicKey,
-          // privateKey
+          privateKey
         });
 
-        if (!publicKeyString) {
+        if (!keyStore) {
           return {
             code: "xxxx",
             message: "publicKeyString error",
           };
         }
-        const publicKeyObject = crypto.createPublicKey(publicKeyString);
         // create token pair
         const tokens = await createTokenPair(
           { userId: newShop._id, email },
-          publicKeyString,
+          publicKey,
           privateKey
         );
         console.log(`Created token success::`, tokens);
