6bac86f0a3edc03539a1a057582e388fcc3edc5c
dangtranvietcuong <cuongcesc12a1@gmail.com>
2024-09-19 09:49:02 +0700

    custom dynamic middleware for api key and permissions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const { apiKey, permission } = require("../auth/checkAuth");
+const router = express.Router();
 
-router.use('/v1/api', require('./access'))
-// router.get('/', (req, res, next) => {
-//     return res.status(200).json({
-//         message: 'Welcome CuongDTV',
-//     })
-// })
+// check apiKey
+router.use(apiKey);
+// check permissions
+router.use(permission('0000'));
 
-module.exports = router
+router.use("/v1/api", require("./access"));
+
+module.exports = router;
